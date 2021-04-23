206ecc0d5fa6932f2111b4241b37a7cac7772238
Tingle <falblennart@gmail.com>
2021-04-23 14:52:13 +0200

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace UnikatParser
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.ErgLabel = new System.Windows.Forms.Label();
+			this.PrimäreTB = new System.Windows.Forms.TextBox();
+			this.SekundäreTB = new System.Windows.Forms.TextBox();
+			this.ButtonStart = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// ErgLabel
+			// 
+			this.ErgLabel.AutoSize = true;
+			this.ErgLabel.Location = new System.Drawing.Point(14, 166);
+			this.ErgLabel.Name = "ErgLabel";
+			this.ErgLabel.Size = new System.Drawing.Size(50, 13);
+			this.ErgLabel.TabIndex = 1;
+			this.ErgLabel.Text = "Unikate: ";
+			// 
+			// PrimäreTB
+			// 
+			this.PrimäreTB.Location = new System.Drawing.Point(17, 78);
+			this.PrimäreTB.Name = "PrimäreTB";
+			this.PrimäreTB.Size = new System.Drawing.Size(391, 20);
+			this.PrimäreTB.TabIndex = 2;
+			this.PrimäreTB.Text = "der pfannkuchen schmeckt süß und nach apfel und apfel";
+			// 
+			// SekundäreTB
+			// 
+			this.SekundäreTB.Location = new System.Drawing.Point(17, 121);
+			this.SekundäreTB.Name = "SekundäreTB";
+			this.SekundäreTB.Size = new System.Drawing.Size(391, 20);
+			this.SekundäreTB.TabIndex = 3;
+			this.SekundäreTB.Text = "der pfannkuchen schmeckt salzig aber auch nach apfel und zitrone";
+			// 
+			// ButtonStart
+			// 
+			this.ButtonStart.Location = new System.Drawing.Point(97, 195);
+			this.ButtonStart.Name = "ButtonStart";
+			this.ButtonStart.Size = new System.Drawing.Size(229, 51);
+			this.ButtonStart.TabIndex = 4;
+			this.ButtonStart.Text = "Start!";
+			this.ButtonStart.UseVisualStyleBackColor = true;
+			this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(422, 258);
+			this.Controls.Add(this.ButtonStart);
+			this.Controls.Add(this.SekundäreTB);
+			this.Controls.Add(this.PrimäreTB);
+			this.Controls.Add(this.ErgLabel);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Label ErgLabel;
+		private System.Windows.Forms.TextBox PrimäreTB;
+		private System.Windows.Forms.TextBox SekundäreTB;
+		private System.Windows.Forms.Button ButtonStart;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnikatParser
+{
+	public partial class Form1 : Form
+	{
+		bool notUnikati, notUnikatl; //Bools für die for schleifen, getrennt nach dem i zähler und dem l zähler
+		bool firstEnter; //bool um out.of.range exceptions zu verhindern. Wurde während des programmierens genutzt. funktioniert mittlerweile auch ohne.
+
+		List<string> ergString = new List<string>(); //Liste in der die unikate zusammengefasst werden
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void ButtonStart_Click(object sender, EventArgs e)
+		{
+			ergString.Clear(); //zum cleanen des labels wenn der button gedrückt wird
+			firstEnter = true;
+			string[] primärString = PrimäreTB.Text.Split(new string[] { " " }, StringSplitOptions.None); //fügt den text in der textbox einem array hinzu, und wird pro word gesplittet wo ein leerzeichen ist
+			string[] sekundärString = SekundäreTB.Text.Split(new string[] { " " }, StringSplitOptions.None);
+
+			for (int i = 0; i < primärString.Length; i++) //die main for schleife der den primärstring mit sich selbst und dem sekundärstring vergleicht.
+			{
+				notUnikatl = true;
+				for (int k = 0; k < sekundärString.Length - 1; k++)
+				{
+					if (primärString[i] == sekundärString[k] && firstEnter) //If beide x.ten wörter in den zwei string sind gleich, dann:
+					{
+						notUnikati = false;
+					}
+					else if (primärString[i] == sekundärString[k] && firstEnter == false)
+					{
+						notUnikati = false;
+					}
+					firstEnter = false;
+				}
+
+				if (notUnikati)
+				{
+					for (int l = 0; l <= primärString.Length - 1; l++) 
+					{
+
+						if (primärString[i] == primärString[l]) //if das x.te wort im selben string zweimal vorkommt, dann:
+						{
+							if (i != l) //Wenn beide NICHT die selbe x.te position haben (sonst würde das programm dasselbe wort als doppelt sehen)
+							{
+								notUnikatl = false;
+							}
+						}
+						else if (primärString[i] == primärString[l])
+						{
+							if (i != l)
+							{
+								notUnikatl = false;
+							}
+						}
+					}
+
+					if (notUnikatl == true) //notUnikatl wird nur false gesetzt wenn der x.te primäre eintrag irgendwo doppelt vorkommt.
+					{
+						ergString.Add(primärString[i]); //fügt den x.ten beitrag aus dem primären string-array in die list hinzu
+					}
+				}
+				notUnikati = true;
+
+			}
+
+			//^ v
+
+			for (int i = 0; i < sekundärString.Length; i++) //die main for schleife der den sekundärstring mit sich selbst und dem primärstring vergleicht. relativ identisch zur oberen for schleife.
+			{
+				notUnikatl = true;
+				for (int k = 0; k < primärString.Length - 1; k++)
+				{
+					if (sekundärString[i] == primärString[k] && firstEnter)
+					{
+						notUnikati = false;
+					}
+					else if (sekundärString[i] == primärString[k] && firstEnter == false)
+					{
+						notUnikati = false;
+					}
+					firstEnter = false;
+				}
+
+				if (notUnikati)
+				{
+					for (int l = 0; l <= sekundärString.Length - 1; l++)
+					{
+						if (sekundärString[i] == sekundärString[l])
+						{
+							if (i != l)
+							{
+								notUnikatl = false;
+							}
+
+						}
+						else if (sekundärString[i] == sekundärString[l])
+						{
+							if (i != l)
+							{
+								notUnikatl = false;
+							}
+						}
+					}
+
+					if (notUnikatl == true)
+					{
+						ergString.Add(sekundärString[i]);
+					}
+				}
+				notUnikati = true;
+
+				if (ergString.Count() != 0) //If ergstring hat mindestens 1 element
+				{
+					ErgLabel.Text = "Unikate: " + string.Join(", ", ergString.ToArray()); ; //zeigt die unikate in ein label an.
+					firstEnter = true;
+				}
+			}
+		}
+	}
+}
